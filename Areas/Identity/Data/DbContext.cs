138f055723109983788940d4baff1c73ec73f07a
juliagawlik33 <juliagawlik27@gmail.com>
2025-12-18 00:35:21 +0100

    Dodaj pliki projektów.

---
+++ Areas/Identity/Data/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelSystem.Areas.Identity.Data;
+
+public class DbContext : IdentityDbContext<User>
+{
+    public DbContext(DbContextOptions<DbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
